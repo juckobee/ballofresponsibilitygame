364d4f588c646c7dfe9a99f257245ce033b24375
Jakub Koba <jkoba@kogifi.com>
2024-09-18 21:53:04 +0200

    Production build optimisation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import './globals.css'
+import { Bangers } from 'next/font/google';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const bangers = Bangers({ 
+  subsets: ['latin'], 
+  weight: '400',
+  variable: '--font-bangers', // Add this line
+});
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${bangers.variable}`}>
       <body>{children}</body>
     </html>
   );
